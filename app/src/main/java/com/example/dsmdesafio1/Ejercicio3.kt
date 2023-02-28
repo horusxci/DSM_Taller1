06abd32cdad12672cb90ebe54ae512cba0e9c003
horusxci <horus91@live.com>
2023-02-27 22:02:36 -0600

    Added menu to main activity

--- app/src/main/java/com/example/dsmdesafio1/Ejercicio3.kt
+++ app/src/main/java/com/example/dsmdesafio1/Ejercicio3.kt
@@ -2,10 +2,34 @@ package com.example.dsmdesafio1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class Ejercicio3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio3)
+
+        var btnOperar = findViewById<Button>(R.id.btnOperar)
+        var operGrp = findViewById<RadioGroup>(R.id.rdioGrpOperadores)
+        var resView = findViewById<TextView>(R.id.txtViewResOper)
+
+        btnOperar.setOnClickListener{
+            var num1:Float = findViewById<EditText>(R.id.edTxtNum1).text.toString().toFloat()
+            var num2:Float = findViewById<EditText>(R.id.edTxtNum2).text.toString().toFloat()
+            var res = ""
+            when(operGrp.checkedRadioButtonId){
+                R.id.rdBtnSuma      -> res = (num1+num2).toString()
+                R.id.rdBtnResta     -> res = (num1-num2).toString()
+                R.id.rdBtnProduct   -> res = (num1*num2).toString()
+                R.id.rdBtnDivision  -> res = if(num2 != 0f) (num1/num2).toString() else "Division entre 0"
+            }
+            resView.text = res
+
+        }
     }
 }
--- app/src/main/java/com/example/dsmdesafio1/MainActivity.kt
+++ app/src/main/java/com/example/dsmdesafio1/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.dsmdesafio1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main,menu)
+        return  true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.menuOp1-> {
+                val intent = Intent(this, Ejercicio1::class.java)
+                startActivity(intent)
+            }
+            R.id.menuOp2-> {
+                val intent = Intent(this, Ejercicio2::class.java)
+                startActivity(intent)
+            }
+            R.id.menuOp3-> {
+                val intent = Intent(this, Ejercicio3::class.java)
+                startActivity(intent)
+            }
+        }
+
+
+
+            startActivity(intent)
+        /*
+
+        when(item.itemId){
+            "menuOp1"->val intent = Intent(this,Ejercicio1::class.java).apply
+            "menuOp2"->val intent = Intent(this,Ejercicio2::class.java).apply
+            "menuOp3"->val intent = Intent(this,Ejercicio3::class.java).apply
+        }
+        startActivity(intent)*/
+        return true
+    }
+
+
 }
